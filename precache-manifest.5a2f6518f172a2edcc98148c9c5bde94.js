bd83ff0972e747b23854f30754084d19c177f55e
Pavol Hejny <me@pavolhejny.com>
2019-12-22 21:49:10 +0100

    Updates

---
+++ precache-manifest.5a2f6518f172a2edcc98148c9c5bde94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541dfbc3cec9216a3b915a1a36972247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecd38345bf84b1cbcbc",
+    "url": "/static/css/main.8190d35d.chunk.css"
+  },
+  {
+    "revision": "0078d7be7ed86cfdd20e",
+    "url": "/static/js/2.ca397a7e.chunk.js"
+  },
+  {
+    "revision": "f21ba130d15371645e57fadce8d0c592",
+    "url": "/static/js/2.ca397a7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aecd38345bf84b1cbcbc",
+    "url": "/static/js/main.22697427.chunk.js"
+  },
+  {
+    "revision": "fe49ede771701fbb3e48",
+    "url": "/static/js/runtime-main.2f8dd714.js"
+  },
+  {
+    "revision": "2ba6dec91edca060a08d58b3b9b24d10",
+    "url": "/static/media/network.2ba6dec9.svg"
+  }
+]);
