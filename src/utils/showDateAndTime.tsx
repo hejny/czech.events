964120296c5a26cb555abe8d37c3666cbb49bfa9
Pavol Hejny <me@pavolhejny.com>
2020-01-01 23:03:46 +0100

    cs lacale

--- src/utils/showDateAndTime.tsx
+++ src/utils/showDateAndTime.tsx
@@ -1,9 +1,22 @@
 import moment from 'moment';
+import 'moment/locale/cs';
+
 export function showDate(date: Date): string {
     try {
         moment.locale('cs');
         let dateString = moment(date).format('LLLL');
         dateString = dateString.split('0:00')[0];
+        //TODO: More elegant way
+        dateString = dateString.replace('leden', 'Ledna');
+        dateString = dateString.replace('únor', 'Února');
+        dateString = dateString.replace('březen', 'Března');
+        dateString = dateString.replace('duben', 'Dubna');
+        dateString = dateString.replace('květen', 'Května');
+        dateString = dateString.replace('červen', 'Června');
+        dateString = dateString.replace('červenec', 'Července');
+        dateString = dateString.replace('srpen', 'Srpna');
+        dateString = dateString.replace('září', 'Září');
+        dateString = dateString.replace('říjen', 'Října');
         dateString = dateString.replace('listopad', 'Listopadu');
         dateString = dateString.replace('prosinec', 'Prosince');
         dateString = dateString.substr(0, 1).toUpperCase() + dateString.substr(1);
