534a30e2a8f3461086ae525813549dcaae71fd98
Pavol Hejny <me@pavolhejny.com>
2020-02-21 16:43:34 +0100

    Repairs

--- src/model/database/AbstractModel.ts
+++ src/model/database/AbstractModel.ts
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
-import { Model as ObjectionModel } from 'objection';
+import { Model } from 'objection';
 
-export class AbstractModel extends ObjectionModel {
+export class AbstractModel extends Model {
     //public static modelPaths = [__dirname];
     //protected static dateFields: string[] = [];
     /*
