858c27e647fd97b744abeb3942a0e4a7a2a0a26f
Pavol Hejný <me@pavolhejny.com>
2020-02-29 15:04:54 +0100

    repairs

--- scripts/generate-database-models.ts
+++ scripts/generate-database-models.ts
@@ -15,9 +15,35 @@ async function main() {
         } catch (error) {
             throw new Error(`You should commit changes first before generating new database models.`);
         }
-        await execAsyncFull(
-            `npx typeorm-model-generator -h ${DB_HOST} -d ${DB_NAME} -u ${DB_USER} -x "${DB_PASSWORD}" -e mysql -o ${baseFolder}`,
-        );
+
+        try {
+            await execAsyncFull(
+                `npx typeorm-model-generator -h ${DB_HOST} -d ${DB_NAME} -u ${DB_USER} -x "${DB_PASSWORD}" -e mysql -o ${baseFolder}`,
+            );
+        } catch (error) {
+            if (error.message.includes('operation not permitted, unlink')) {
+                // TODO: weird error on Windows
+                /*
+                Error: EPERM: operation not permitted, unlink 'C:\Users\me\AppData\Roaming\npm-cache\_npx\11612\node_modules\typeorm-model-generator\node_modules\sqlite3\lib\binding\node-v72-win32-x64\node_sqlite3.node'        
+                    at Object.unlinkSync (fs.js:976:3)
+                    at fixWinEPERMSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:211:13)
+                    at rimrafSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:311:28)
+                    at C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:342:5
+                    at Array.forEach (<anonymous>)
+                    at rmkidsSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:341:26)
+                    at rmdirSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:334:7)
+                    at fixWinEPERMSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:209:5)
+                    at rimrafSync (C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:311:28)
+                    at C:\Program Files\nodejs\node_modules\npm\node_modules\rimraf\rimraf.js:342:5 {
+                errno: -4048,
+                syscall: 'unlink',
+                code: 'EPERM',
+                path: 'C:\\Users\\me\\AppData\\Roaming\\npm-cache\\_npx\\11612\\node_modules\\typeorm-model-generator\\node_modules\\sqlite3\\lib\\binding\\node-v72-win32-x64\\node_sqlite3.node'
+                */
+            } else {
+                throw error;
+            }
+        }
 
         console.info('Database models successfully created, please purge them before commit by:');
         console.warn('\x1b[43m', '\x1b[37m', `rm -rf ${baseFolder}`, '\x1b[0m');
