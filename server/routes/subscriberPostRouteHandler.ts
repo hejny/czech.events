646a0acaba14d46d3de046b019244882696cf407
Pavol Hejný <me@pavolhejny.com>
2020-04-06 13:34:31 +0200

    subscriber.uuid = uuid.v4();

--- server/routes/subscriberPostRouteHandler.ts
+++ server/routes/subscriberPostRouteHandler.ts
@@ -2,12 +2,14 @@ import { Subscriber } from '../../src/model/database/Subscriber';
 import { constructObjectFromJSON } from '../../src/utils/constructObjectFromJSON';
 import { RequestHandler } from 'express';
 import { connectionPromise } from '../database';
+import uuid from 'uuid';
 
 export const subscriberPostRouteHandler: RequestHandler = async (request, response, next) => {
     const connection = await connectionPromise;
     // TODO: Purge internal IDs
     const subscriber = constructObjectFromJSON(Subscriber, request.body);
     subscriber.created = new Date();
+    subscriber.uuid = uuid.v4();
     const insertResult = await connection.manager.insert(Subscriber, subscriber);
 
     if (insertResult.identifiers.length === 1) {
