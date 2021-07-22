9f72b4aa36696741a1c6ccdf29d92afdf1cb0ce6
Pavol Hejny <me@pavolhejny.com>
2021-07-23 01:59:55 +0200

    Range in exports

--- server/routes/getExportHtmlRouteHandler.tsx
+++ server/routes/getExportHtmlRouteHandler.tsx
@@ -1,15 +1,14 @@
 import { RequestHandler } from 'express';
-import * as React from 'react';
 import { promises as fsp } from 'fs';
 import { join } from 'path';
 import prettier from 'prettier';
+import * as React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { NewsletterComponent } from '../../src/components/NewsletterComponent';
-import { createNewsletter } from '../../src/utils/createNewsletter';
 import { In } from 'typeorm';
-
+import { NewsletterComponent } from '../../src/components/NewsletterComponent';
 import { Event, EventVisibility } from '../../src/model/database/Event';
 import { DateRange } from '../../src/model/DateRange';
+import { createNewsletter } from '../../src/utils/createNewsletter';
 import { connectionPromise } from '../database';
 
 const { readFile } = fsp; // TODO: } from 'fs/promises';
@@ -22,7 +21,9 @@ export const getExportHtmlRouteHandler: RequestHandler = async (request, respons
     // TODO: Purge internal IDs
     // TODO: Remove codes
 
-    const range = DateRange.fromConstants('CURRENT_MONTH', 'NEXT_MONTH');
+    const rangeString = request.query.range; /* TODO: when vs. range */
+
+    const range = DateRange.fromConstant((rangeString as any) || 'CURRENT_MONTH-NEXT_MONTH');
     const newsletter = createNewsletter({ range, events });
     const content = ReactDOMServer.renderToStaticMarkup(<NewsletterComponent {...{ newsletter }} />);
     const style = await readFile(join(__dirname, '../../src/style/newsletter.css'));
