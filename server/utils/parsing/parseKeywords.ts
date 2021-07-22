b8d7d9595bcb392237b286e087596216e8855329
Pavol Hejny <me@pavolhejny.com>
2021-07-22 15:37:14 +0200

    TODO

--- server/utils/parsing/parseKeywords.ts
+++ server/utils/parsing/parseKeywords.ts
@@ -1,5 +1,6 @@
 import { ISemanticEvent } from '../../interfaces/jsonld/ISemanticEvent';
 
+// TODO: Use instead i12
 export function parseKeywords({ semanticEvent }: { semanticEvent: ISemanticEvent }): { keywords: string[] } {
     // TODO: normalize !! - toLowerCase also for ěščřžýáíéúů
     const keywords = `${semanticEvent.name} ${semanticEvent.description}`.toLowerCase().split(/\s+/);
