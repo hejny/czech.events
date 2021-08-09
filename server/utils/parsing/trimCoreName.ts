6e2cde9dadf1437d9c867f1e217bd3ee263da0c1
Pavol Hejny <me@pavolhejny.com>
2021-08-09 13:26:40 +0200

    Repair trimCoreName

--- server/utils/parsing/trimCoreName.ts
+++ server/utils/parsing/trimCoreName.ts
@@ -1,7 +1,7 @@
 export function trimCoreName(fullName: string): string {
     // DRY [^a-zA-Z0-9ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ]+
     fullName = fullName.replace(/^[^a-zA-Z0-9ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ]+/, '');
-    fullName = fullName.replace(/[^a-zA-Z0-9ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ]+[ia]?[^a-zA-Z0-9ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ]+$/, '');
+    fullName = fullName.replace(/[^a-zA-Z0-9ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ]+$/, '');
 
     // TODO: Not a pure trim
     fullName = fullName.replace(`"`, '');
