a9a8abbeb3c96e5795a353bbd4ae82a31ad6be5d
Pavol Hejny <me@pavolhejny.com>
2021-08-09 14:43:21 +0200

    Better parsing of the city from semantic data

--- server/utils/parsing/city/parseCity.ts
+++ server/utils/parsing/city/parseCity.ts
@@ -2,50 +2,49 @@ import { ISemanticEvent } from '../../../interfaces/jsonld/ISemanticEvent';
 import { CITIES } from './CITIES';
 import { normalizeCity } from './normalizeCity';
 
-export function parseCity({ semanticEvent, keywords }: { semanticEvent: ISemanticEvent; keywords: string[] }) {
-    let city: null | string = null;
-    if (semanticEvent?.location?.address?.addressLocality) {
-        city = semanticEvent.location.address.addressLocality;
+export function parseCity({
+    semanticEvent,
+    keywords,
+}: {
+    semanticEvent: ISemanticEvent;
+    keywords: string[];
+}): { city: string | null } {
+    for (const possibleCity of [
+        semanticEvent?.location?.name,
+        semanticEvent?.location?.address?.addressLocality,
+    ].filter((adress) => adress !== null && adress !== undefined)) {
+        // console.log({ possibleCity });
         // TODO: Normalize aleternatives from CITIES
 
-        const normalizedCity = normalizeCity(city);
+        const normalizedCity = normalizeCity(possibleCity);
 
         if (normalizedCity) {
-            city = normalizedCity;
+            return { city: normalizedCity };
         } else {
-            console.warn(`City "${city}" is not in the list.`);
+            console.warn(`City "${possibleCity}" is not in the list.`);
         }
     }
 
-    if (typeof city !== 'string') {
-        city = null;
-    }
+    const citiesFromKeywords: Set<string> = new Set();
 
-    if (!city) {
-        const citiesFromKeywords: Set<string> = new Set();
+    for (const [key, alternatives] of Object.entries(CITIES)) {
+        if (keywords.includes(key.toLowerCase())) {
+            citiesFromKeywords.add(key);
+        }
 
-        for (const [key, alternatives] of Object.entries(CITIES)) {
-            if (keywords.includes(key.toLowerCase())) {
+        for (const alternative of alternatives) {
+            if (keywords.includes(alternative.toLowerCase())) {
                 citiesFromKeywords.add(key);
             }
-
-            for (const alternative of alternatives) {
-                if (keywords.includes(alternative.toLowerCase())) {
-                    citiesFromKeywords.add(key);
-                }
-            }
         }
-
-        if (citiesFromKeywords.size === 1) {
-            city = [...citiesFromKeywords][0];
-        } else if (citiesFromKeywords.size > 1) {
-            console.warn(`There are more cities parsed from keywords ${[...citiesFromKeywords].join(', ')}.`);
-        }
-
-        // console.log({ CITIES, keywords, citiesFromKeywords, city });
     }
 
-    // TODO: Pilsen easter egg
+    if (citiesFromKeywords.size === 1) {
+        return { city: [...citiesFromKeywords][0] };
+    } else if (citiesFromKeywords.size > 1) {
+        console.warn(`There are more cities parsed from keywords ${[...citiesFromKeywords].join(', ')}.`);
+        return { city: null };
+    }
 
-    return { city };
+    return { city: null };
 }
