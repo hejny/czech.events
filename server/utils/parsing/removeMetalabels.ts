8d169db5865e5756c5b9304112a48968c11e29a5
Pavol Hejny <me@pavolhejny.com>
2021-09-09 13:03:44 +0200

    Repair tests of removeMetalabels

--- server/utils/parsing/removeMetalabels.ts
+++ server/utils/parsing/removeMetalabels.ts
@@ -1,5 +1,8 @@
 export function removeMetalabels(sentence: string): string {
-    return sentence
-        .replace(/(canceled|zrušeno|online|offline|stream|vysílání|virtuální|virtual|prezenčně|live|(prez\.)|czsk)/gi, '')
+    return ` ${sentence} `
+        .replace(
+            /(?<=[\s.])(canceled|zrušeno|online|offline|stream|vysílání|virtuální|virtual|prezenčně|live|(prez\.)|czsk)(?=[\s.])/gim,
+            '',
+        )
         .trim(); // Note: Removing other keywords // TODO: DRY
 }
