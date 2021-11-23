9c04746cd83f2e58dddfc2a0b609d10b42dbf311
Pavol Hejny <me@pavolhejny.com>
2021-11-23 21:40:49 +0100

    Better ical export

--- server/routes/getExportIcalRouteHandler.tsx
+++ server/routes/getExportIcalRouteHandler.tsx
@@ -1,24 +1,35 @@
 import { RequestHandler } from 'express';
 import ical from 'ical-generator';
 import moment from 'moment';
-import { In } from 'typeorm';
+import { spaceTrim } from 'spacetrim';
+import { FindConditions, In } from 'typeorm';
 import { Event, EventVisibility } from '../../src/model/database/Event';
 import { connectionPromise } from '../database';
 
 export const getExportIcalRouteHandler: RequestHandler = async (request, response, next) => {
-    const extension = request.path.match(/\.(?<extension>[a-zA-Z0-9]+)$/)?.groups?.extension;
-    const mimeType = extension === 'txt' ? 'text/plain' : 'text/calendar';
+    const serializeId = request.query.serializeId as string | undefined;
+    const { filename, extension } = request.path.match(/(?<filename>[a-zA-Z0-9\-_+]+\.?(?<extension>[a-zA-Z0-9]+))$/)
+        ?.groups || {
+        filename: 'czech.events.ics',
+        extension: 'ics',
+    };
+    const mimeType = extension === 'ics' ? 'text/calendar' : 'text/plain';
 
     try {
         const connection = await connectionPromise;
+
+        const where: FindConditions<Event> = { visibility: In([EventVisibility.FEATURED, EventVisibility.VISIBLE]) };
+        if (serializeId) {
+            where.serializeId = serializeId;
+        }
         const events = await connection.manager.find(Event, {
-            where: { visibility: In([EventVisibility.FEATURED, EventVisibility.VISIBLE]) },
+            where,
         });
 
         const calendar = ical({ name: 'Czech.events', prodId: '//pavolhejny.com//Czech.events//EN' });
 
         for (const event of events) {
-            console.log(event.name, event.date);
+            //console.log(event.name, event.date);
             try {
                 if (event.date === null) {
                     continue;
@@ -31,8 +42,16 @@ export const getExportIcalRouteHandler: RequestHandler = async (request, respons
                     // TODO: Some universal util how to join featured+name+topic into summary
                     summary: event.name + ' – ' + event.topic,
 
-                    // !! TODO: Scrape descriptions
-                    // !! description: '',
+                    // !! TODO: Scrape descriptions + Convert React at letter to plain text
+                    description: spaceTrim(`
+                    
+                        
+                        ${event.web}
+                        ______
+                        🌍 Find more events at https://czech.events/
+                    
+                    
+                    `),
                     location: event.city,
                     url: event.web,
                     id: event.serializeId /* TODO: !!! Make here some normalization + mix with Czech events */,
@@ -46,8 +65,8 @@ export const getExportIcalRouteHandler: RequestHandler = async (request, respons
         // TODO: Download as some nice branded filename
         return response
             .type(mimeType)
-            .header('Content-Disposition', 'inline')
-            .send(calendar.toString() /*.replace(/^PRODID\:.*$/m, 'PRODID:-//czech.events/')*/);
+            .header('Content-Disposition', extension === 'txt' ? 'inline' : `attachment; filename="${filename}"`)
+            .send(calendar.toString());
     } catch (error) {
         // console.error(error);
         return response.status(400).send({ error: { name: error.name, message: error.message, ...error } });
