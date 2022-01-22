98b5dd8447e872754013fed4ee5411a23e6584c6
Pavol Hejny <me@pavolhejny.com>
2022-01-22 20:06:20 +0100

    Collboard as a partner

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { shuffleArray } from '../utils/array';
 
@@ -11,36 +10,44 @@ export function Partners(props: IPartnersProps) {
     return (
         <>
             <PartnersDiv className="partners">
-                <h2>Mediální partneři</h2>
-                {shuffleArray([
-                    // TODO: To some other file or even to DB
-                    {
-                        name: 'Startup Weekend Prague',
-                        logoUrl: `${props.selfUrl}/design/logos/partners/startup-weekend-prague.png`,
-                        link: 'https://www.facebook.com/swprague/',
-                    },
+                <h2>Partneři</h2>
 
-                    {
-                        name: 'Startup Weekend Bratislava',
-                        logoUrl: `${props.selfUrl}/design/logos/partners/startup-weekend-bratislava.png`,
-                        link: 'https://www.facebook.com/StartupWeekendBratislava/',
-                    },
-                    {
-                        name: 'Undout Sleep Box',
-                        logoUrl: `${props.selfUrl}/design/logos/partners/undout.png`,
-                        link: 'https://undout.com/',
-                    },
-                    {
-                        name: 'HackPrague',
-                        logoUrl: `${props.selfUrl}/design/logos/partners/hackprague.svg`,
-                        link: 'https://hackprague.com/',
-                    },
-                    {
-                        name: 'StartupBox',
-                        logoUrl: `${props.selfUrl}/design/logos/partners/startupbox.png`,
-                        link: 'https://www.startupbox.cz/',
-                    },
-                    /*
+                <div className="partners">
+                    {shuffleArray([
+                        // TODO: To some other file or even to DB
+                        {
+                            name: 'Startup Weekend Prague',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/startup-weekend-prague.png`,
+                            link: 'https://www.facebook.com/swprague/',
+                        },
+
+                        {
+                            name: 'Startup Weekend Bratislava',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/startup-weekend-bratislava.png`,
+                            link: 'https://www.facebook.com/StartupWeekendBratislava/',
+                        },
+                        {
+                            name: 'Undout Sleep Box',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/undout.png`,
+                            link: 'https://undout.com/',
+                        },
+                        {
+                            name: 'HackPrague',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/hackprague.svg`,
+                            link: 'https://hackprague.com/',
+                        },
+                        {
+                            name: 'StartupBox',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/startupbox.png`,
+                            link: 'https://www.startupbox.cz/',
+                        },
+                        {
+                            name: 'Collboard',
+                            logoUrl: `${props.selfUrl}/design/logos/partners/collboard.png`,
+                            link: 'https://www.collboard.com/',
+                        },
+
+                        /*
                     TODO:
                     {
                         name: 'CEEHACKS',
@@ -52,27 +59,31 @@ export function Partners(props: IPartnersProps) {
                         logoUrl: `${props.selfUrl}/design/logos/partners/techheaven.svg`,
                         link: 'https://techheaven.org/',
                     },*/
-                    // TODO: CzechFuture tech
-                    // TODO: Random shuffle
-                ]).map(({ name, logoUrl, link }) => {
-                    return (
-                        <a
-                            href={`${link}?utm_source=czech.events&amp;utm_medium=referral&amp;utm_campaign=partners`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            title={name}
-                        >
-                            <div key={name} className="logo" style={{ backgroundImage: `url(${logoUrl})` }}>
-                                {/* <img src={logoUrl} title={name} alt={`${name} logo`} /> */}
-                                {/*`${name} `*/}
-                            </div>
-                        </a>
-                    );
-                })}
+                        // TODO: CzechFuture tech
+                        // TODO: Random shuffle
+                    ]).map(({ name, logoUrl, link }) => {
+                        return (
+                            <a
+                                href={`${link}?utm_source=czech.events&amp;utm_medium=referral&amp;utm_campaign=partners`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title={name}
+                            >
+                                {' '}
+                                <div key={name} className="partner">
+                                    <div className="logo" style={{ backgroundImage: `url(${logoUrl})` }}></div>
+                                    {name}
+                                </div>
+                            </a>
+                        );
+                    })}
+                </div>
 
+                {/*
                 <Link to="/about" className="our-logos">
-                    O nás a loga Czech.events
+                    O nás
                 </Link>
+                 */}
             </PartnersDiv>
         </>
     );
@@ -90,16 +101,35 @@ export const PartnersDiv = styled.div`
         font-family: 'Montserrat', serif;
     }
 
-    .logo {
+    .partners {
+        /*/
+        border: 1px dotted red;
+        /**/
+
+        display: flex;
+    }
+
+    .partner {
+        /*/
+        border: 1px dotted red;
+        /**/
+
         margin: 20px;
-        border-radius: 5px;
+
+        width: 100px;
         display: inline-block;
+        color: white;
+        font-size: 0.9rem;
+    }
+
+    .logo {
         width: 100px;
         height: 100px;
-        color: white;
+        border-radius: 7px;
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
+        margin-bottom: 10px;
     }
 
     .our-logos {
