32dd48a5522ae44da6c6a7cfe33ea4ce5c1c1e67
Pavol Hejny <me@pavolhejny.com>
2022-11-18 14:35:37 +0100

    Removing reference to deprecated util

--- src/components/EventPrice.tsx
+++ src/components/EventPrice.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import { Event } from '../model/database/Event';
-import { isNullOrUndefined } from 'util';
 import { translateCurrency } from '../utils/translate';
 
 interface IEventPriceProps {
@@ -11,7 +9,7 @@ interface IEventPriceProps {
 // TODO: Component name suffix to all components
 export function EventPrice({ event, price }: IEventPriceProps) {
     if (!price) price = event.price;
-    if (isNullOrUndefined(price)) return <></>;
+    if (price === null || price === undefined) return <></>;
     if (price === 0) return <>💸&nbsp;Zdarma</>;
     return <>💸&nbsp;{`${Math.ceil(price * 100) / 100} ${translateCurrency(event.priceCurrency!)}`}</>;
 }
