7798f07a79544d94a218236ab6fa2ec70893d39e
Pavol Hejny <me@pavolhejny.com>
2023-02-06 15:53:07 +0100

    Fix deployment script

--- scripts/deploy/deployApi.js
+++ scripts/deploy/deployApi.js
@@ -29,7 +29,17 @@ export async function deployApi(remote) {
 
     /**/
     await client.exec(`/bin/cp -R ${remote.configDir}/. ${remoteDir}`);
-    await client.exec(`npm ci`, false);
+    await client.exec(
+        `npm i` /* <- TODO: Here should be npm ci (clean install) but there is a problem with it:
+                            > npm WARN prepare removing existing node_modules/ before installation
+                            > npm ERR! cipm can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync. Please update your lock file with `npm install` before continuing.
+                            > npm ERR!
+                            > npm ERR!
+                            > npm ERR! Invalid: lock file's @types/node@18.11.19 does not satisfy @types/node@18.11.9
+                            > npm ERR!
+        */,
+        false,
+    );
 
     await client.exec(`pm2 stop czech-events`, false);
     // TODO: !!> await client.exec(`npm test`);
