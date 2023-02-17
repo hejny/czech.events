cf9512979ce7d9c2f8ea231de87fd73c10d76baf
Pavol Hejny <me@pavolhejny.com>
2023-02-17 13:44:12 +0100

    Organize newsletter CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import '../style/global.css' /* <- TODO: Where whould be this file and from wher
 import '../style/newsletter.css' /* <- TODO: Remove global styles */;
 import '../style/body.css' /* <- TODO: Remove global styles */;
 import '../style/page.css' /* <- TODO: Remove global styles */;
-import '../style/event.css' /* <- TODO: Remove global styles */;
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
