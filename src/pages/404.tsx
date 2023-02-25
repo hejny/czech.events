6bc06583eef64431f158cc593f6118fa2eeef57b
Pavol Hejny <me@pavolhejny.com>
2023-02-25 15:58:19 +0100

    Fix 404page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,16 +3,13 @@ import Head from 'next/head';
 import { TalksPage } from '../components/TalksPage/TalksPage';
 import { useMemo } from 'react';
 import { ApiClient } from '../api/ApiClient';
-import { HeadSection } from '../components/AppHead/AppHead';
+import { AppHead, HeadSection } from '../components/AppHead/AppHead';
 import { Footer } from '../components/Footer/Footer';
 
 export default function NotFoundPage() {
     return (
         <>
-            <Head>
-                <HeadSection subtitle="Nenalezeno" />
-                {/* !!! Better and nicer */}
-            </Head>
+            <AppHead subtitle="Nenalezeno" />
             <div className="page">404 {/* !!! Better and nicer */}</div>
             <Footer />
         </>
