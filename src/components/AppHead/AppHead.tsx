8f7b219d17271a1adbf8e67d9ee99ab026616c0d
Pavol Hejny <me@pavolhejny.com>
2023-03-05 18:21:47 +0100

    Fix title element

--- src/components/AppHead/AppHead.tsx
+++ src/components/AppHead/AppHead.tsx
@@ -13,7 +13,7 @@ export function AppHead(props: AppHeadSectionProps) {
     return (
         <>
             <Head>
-                <title>{`Czech.events | ${subtitle}`}</title>
+                <title>{`${!subtitle ? `` : `${subtitle} 📆 `}Czech.events`}</title>
                 <meta
                     name="description"
                     content="Mějte přehled o nejzajímavějších událostech z IT & startupového světa."
@@ -43,8 +43,6 @@ export function AppHead(props: AppHeadSectionProps) {
 }
 
 /**
- * TODO: !!! Warning: A title element received an array with more than 1 element as children. In browsers title Elements can only have Text Nodes as children. If the children being rendered output more than a single text node in aggregate the browser will display markup
-and comments as text in the title and hydration will likely fail and fall back to client rendering
  * TODO: Is Google Analytics working
  * TODO: !!! Real favicon
  * TODO: Dark and light favicon
